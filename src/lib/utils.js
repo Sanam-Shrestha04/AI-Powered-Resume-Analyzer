c5f2856ea3c35a19b5c3d8d0dee1dd02d0115227
Sanam-Shrestha04 <sthasanam067@gmail.com>
2025-11-13 23:12:50 +0545

    Resume Feedback componts added

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,9 +1,15 @@
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs) {
+  return twMerge(clsx(...inputs));
+}
 
 export function formatSize(bytes) {
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-  if (bytes === 0) return '0 Bytes';
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+  if (bytes === 0) return "0 Bytes";
   const i = Math.floor(Math.log(bytes) / Math.log(1024));
-  return parseFloat((bytes / Math.pow(1024, i)).toFixed(2)) + ' ' + sizes[i];
+  return parseFloat((bytes / Math.pow(1024, i)).toFixed(2)) + " " + sizes[i];
 }
 
 export const generateUUID = () => crypto.randomUUID();
